f37e99be5429c4bc10e38cdf813b3fa21129b7bb
Oskitar-Ale <108702192+Oskitar-Ale@users.noreply.github.com>
2023-08-11 00:00:01 -0500

    final update

--- postcard/js/index.js
+++ postcard/js/index.js
@@ -1,30 +1,3 @@
-const form = document.querySelector('.text-to');
-const input =  document.querySelectorAll('input');
-
-form.addEventListener('submit', () => {
-    event.preventDefault();
-    const name = document.getElementById("name").value;
-    const city = document.getElementById("city").value;
-    const description = document.getElementById("description").value;
-
-    const formData = {
-        name,
-        city,
-        description,
-    }
-
-    console.log(formData);
-
-    const registro = document.querySelector('.registro');  
-    registro.innerHTML = `
-    <p>Nombre: ${formData.name}<p>
-    <p>City: ${formData.city}<p>
-    <p>Description: ${formData.description}<p>
-    `
-
-})
-
-
 const flip = document.getElementById("flip");
 const postcard = document.querySelector('.main');
 flip.addEventListener('click', () => {
@@ -42,3 +15,22 @@ btnClear.addEventListener('click', () => {
         input.value = '';
     })
 })
+
+const containerBg = document.querySelector('.gradient');
+const bg = document.querySelector('#bg');
+
+const colors = [
+    'linear-gradient(45deg, #31966e, #18368e, #a9baf8)',
+    'linear-gradient(45deg, #4051d7, #6cf3bb, #b522ed)',
+    'linear-gradient(to right top, #86a8e7, #86a8e7, #86a8e7, #86a8e7, #86a8e7, #7db0ee, #73b7f3, #69bff8, #52cffe, #41dfff, #46eefa, #5ffbf1)',
+    'linear-gradient(0deg, #a20aa0, #ca442c, #957b5f)',
+    'linear-gradient(0deg, #b92165, #3836df, #ef34cd)',
+    'linear-gradient(225deg, #fb725c, #f23889, #e52646)'
+];
+
+let index = 0;
+
+bg.addEventListener('click', () => {
+    index = (index + 1) % colors.length;
+    containerBg.style.background = colors[index];
+});
